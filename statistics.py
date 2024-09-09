fc6bc7bf98b0bebe01f360ccbe1c7d9c19d6c872
Pawat Sarnchawanakit <pawat.sar@ku.th>
2024-09-09 14:04:30 +0700

    Fixed formatting for `statistics.py`

--- statistics.py
+++ statistics.py
@@ -1,14 +1,16 @@
+"""Implement statistics functions."""
 import math
+
 
 def average(data):
     """Return the average of a list of numeric values in data."""
     if len(data) == 0:
         raise ValueError("List must contain at least one value")
-    return sum(data)/len(data)
+    return sum(data) / len(data)
 
 
 def variance(data):
-    """the population variance of a list of numbers in data.
+    """Find the population variance of a list of numbers in data.
 
     The variance is the sum of squared differences between data values
     and their mean, divided by the number of items in the list.
@@ -17,7 +19,7 @@ def variance(data):
 
     Example: variance([1,5]) is ((1-3)**2 + (5-3)**2)/2 = 4.
 
-    :param data: list of numbers for which variance will be computed. 
+    :param data: list of numbers for which variance will be computed.
            Must contain at least one element.
     :returns: population variance of values in data list.
     :raises ValueError: if the data parameter is empty.
@@ -35,10 +37,9 @@ def variance(data):
     if n == 0:
         raise ValueError("List must contain at least one value")
     avg = average(data)
-    return round(sum((x-avg)**2 for x in data)/n, 14)
+    return round(sum((x - avg)**2 for x in data) / n, 14)
 
 
 def stdev(data):
-    """the standard deviation of a list of values"""
+    """Find the standard deviation of a list of values."""
     return math.sqrt(variance(data))
-
